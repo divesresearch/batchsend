90a866b9bf7058bdc0141dc0f8293678d1e975c4
Ugur Erdem <k.ugurerdem@gmail.com>
2023-02-05 18:31:10 +0300

    Implement batchSend method in BatchSenderERC20.sol

--- src/contracts/BatchSenderERC20.sol
+++ src/contracts/BatchSenderERC20.sol
@@ -3,25 +3,27 @@ pragma solidity 0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
 contract BatchSenderERC20 {
-    function transferFrom(
+
+    function transferTokenFrom(
         IERC20 token,
         address targetAddress,
         uint256 amount
     ) private returns (bool) {
         require(
             token.balanceOf(msg.sender) >= amount,
-            "insufficient balance"
+            "insufficient token balance"
         );
 
         token.transferFrom(msg.sender, targetAddress, amount);
         return true;
     }
 
-    function multiSendToken(
-        address tokenAddress, 
+    function batchSendToken(
+        address tokenAddress,
         address[] memory recipients,
         uint256[] memory amounts
     ) public returns (bool) {
+
         require(recipients.length > 0);
         require(recipients.length == amounts.length);
 
@@ -29,7 +31,23 @@ contract BatchSenderERC20 {
 
         uint256 length = recipients.length;
         for(uint i = 0; i < length; i++){
-            transferFrom(token, recipients[i], amounts[i]);
+            transferTokenFrom(token, recipients[i], amounts[i]);
+        }
+
+        return true;
+    }
+
+    function batchSend(
+        address payable[] memory recipients,
+        uint256[] memory amounts
+    ) payable public returns (bool){
+
+        require(recipients.length > 0);
+        require(recipients.length == amounts.length);
+
+        uint256 length = recipients.length;
+        for(uint i = 0; i < length; i++){
+            recipients[i].transfer(amounts[i]);
         }
 
         return true;
